e5244fda62e8eb44352dbf504dfc76e559cfe56b
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-08-10 10:21:07 +0700

    Initial Commit

---
+++ UISlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISlider
+//
+//  Created by Do Minh Hai on 8/10/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISlider
+//
+//  Created by Do Minh Hai on 8/10/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
